fd870431c19aec588df981549936b6e1c2cb5524
Nikita Koynov <nikicha03@gmail.com>
2023-10-07 00:39:29 +0300

    guess slots

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {range} from "../../utils";
+
+function Guess({value}) {
+  return (
+    <p className="guess">
+      {range(5).map((i) => <span key={i} className="cell">{value?.[i]}</span>)}
+    </p>
+  );
+}
+
+export default Guess;
